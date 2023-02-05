2bfb3c5c36fd0753f2a50776319585a35349f17a
Omar Taha <omartaha.tech7@gmail.com>
2023-02-05 13:49:34 +0300

    Initial commit

---
+++ lib/super_flutter.dart
@@ -0,0 +1,8 @@
+library super_flutter;
+
+import 'package:super_flutter/super_flutter.dart';
+export 'package:super_localizations/super_localizations.dart';
+export 'package:super_repository/super_repository.dart';
+
+part 'error/errors.dart';
+part 'error/firebase_errors.dart';
